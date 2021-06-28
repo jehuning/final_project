a2029c4ef8acd81c1eead517386c94d092bcbc4d
JEHUNinG <JEHUNinG@JEHUNinG-PC>
2021-06-28 22:25:59 +0900

    0628jh1

---
+++ pugis/src/main/java/com/kh/pugis/service/consulting/utils/Test.java
@@ -0,0 +1,10 @@
+package com.kh.pugis.service.consulting.utils;
+
+public class Test {
+
+	public static void main(String[] args) {
+		CalcWorkDay cwd = new CalcWorkDay();
+		System.out.println(cwd.calcWorkDay().size());
+	}
+
+}
