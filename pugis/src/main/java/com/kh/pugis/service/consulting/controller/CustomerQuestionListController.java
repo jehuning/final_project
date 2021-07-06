6e1506be63a813a5e0fb81b17274e39c00d35306
YOUNGSIM <imyoung0127@gmail.com>
2021-07-06 16:08:48 +0900

    web.xml 한글 인코딩 코드 추가 src/main/resources mybatis-config.xml 파일 추가 customerquestionlist 작성 (게시글 등록 및 삭제X, 답글 삭제X, 카테고리 버튼X 추후 수정)

--- pugis/src/main/java/com/kh/pugis/service/consulting/controller/CustomerQuestionListController.java
+++ pugis/src/main/java/com/kh/pugis/service/consulting/controller/CustomerQuestionListController.java
@@ -1,35 +1,123 @@
 package com.kh.pugis.service.consulting.controller;
 
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.kh.pugis.service.consulting.domain.Criteria;
+import com.kh.pugis.service.consulting.domain.CustomerQuestion;
+import com.kh.pugis.service.consulting.domain.CustomerQuestionReply;
+import com.kh.pugis.service.consulting.domain.PageMaker;
+import com.kh.pugis.service.consulting.domain.SearchCriteria;
+import com.kh.pugis.service.consulting.service.CustomerQuestionListReplyService;
 import com.kh.pugis.service.consulting.service.CustomerQuestionListService;
-
-
 
 @Controller
 @RequestMapping(value = "/qna")
-public class CustomerQuestionListController
-{	
-	@Autowired
-    CustomerQuestionListService cs;
-
-
-	@RequestMapping(value = "/list")
-	public void customerQuestionlist(){
-	}
-	@RequestMapping(value = "/detail")
-	public void customerQuestionDetail(){
+public class CustomerQuestionListController {
 	
-	}
-	@RequestMapping(value = "/page")
-	public void customerQuestionPageMove(){
+	@Autowired
+	CustomerQuestionListService cs;
+	
+	@Autowired
+	CustomerQuestionListReplyService rs;
+
+	// 게시글 목록 조회
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public String list(Model model, @ModelAttribute("scri") SearchCriteria scri) throws Exception{
+	//	logger.info("list");
+		
+		model.addAttribute("list", cs.list(scri));
+		
+		PageMaker pageMaker = new PageMaker();
+		pageMaker.setCri(scri);
+		pageMaker.setTotalCount(cs.listCount(scri));
+		
+		model.addAttribute("pageMaker", pageMaker);
+		
+		return "service/consulting/customerquestionlist_back";
 		
 	}
-	@RequestMapping(value = "/search")
-	public void customerQuestionSearch(){
+	
+	// 게시글 조회
+	@RequestMapping(value = "/readView", method = RequestMethod.GET)
+	public String read(CustomerQuestion cqVO, @ModelAttribute("scri") SearchCriteria scri, Model model) throws Exception{
+	//	logger.info("read");
 		
+		model.addAttribute("read", cs.read(cqVO.getReg_id()));
+		model.addAttribute("scri", scri);
+		
+		List<CustomerQuestionReply> replyList = rs.readReply(cqVO.getReg_id());
+		model.addAttribute("replyList", replyList);
+		
+		return "service/consulting/readView_back";
+	}
+
+	// 게시글 삭제
+	@RequestMapping(value = "/delete", method =  {RequestMethod.POST, RequestMethod.GET})
+	public String delete(CustomerQuestion cqVO, @ModelAttribute("scri") SearchCriteria scri, RedirectAttributes rttr) throws Exception{
+	//	logger.info("delete");
+		
+		cs.delete(cqVO.getReg_id());
+		
+		rttr.addAttribute("page", scri.getPage());
+		rttr.addAttribute("perPageNum", scri.getPerPageNum());
+		rttr.addAttribute("searchType", scri.getSearchType());
+		rttr.addAttribute("keyword", scri.getKeyword());
+		
+		return "redirect:/qna/list";
+	}
+	
+	//댓글 작성
+	@RequestMapping(value="/replyWrite", method = {RequestMethod.POST, RequestMethod.GET})
+	public String replyWrite(CustomerQuestionReply cqrVO, SearchCriteria scri, RedirectAttributes rttr) throws Exception {
+	//	logger.info("reply Write");
+		
+		rs.writeReply(cqrVO);
+		
+		rttr.addAttribute("reg_id", cqrVO.getReg_id());
+		rttr.addAttribute("page", scri.getPage());
+		rttr.addAttribute("perPageNum", scri.getPerPageNum());
+		rttr.addAttribute("searchType", scri.getSearchType());
+		rttr.addAttribute("keyword", scri.getKeyword());
+		
+		return "redirect:/qna/readView";
+	}
+
+	
+	//댓글 삭제 GET
+	@RequestMapping(value="/replyDeleteView", method = RequestMethod.GET)
+	public String replyDeleteView(CustomerQuestionReply cqrVO, SearchCriteria scri, Model model) throws Exception {
+	//	logger.info("reply Write");
+		
+		model.addAttribute("replyDelete", rs.selectReply(cqrVO.getReply_id()));
+		model.addAttribute("scri", scri);
+		
+
+		return "service/consulting/replyDeleteView_back";
+	}
+	
+	//댓글 삭제
+	@RequestMapping(value="/replyDelete", method = RequestMethod.POST)
+	public String replyDelete(CustomerQuestionReply cqrVO, SearchCriteria scri, RedirectAttributes rttr) throws Exception {
+	//	logger.info("reply Write");
+		
+		rs.deleteReply(cqrVO);
+		
+		rttr.addAttribute("reg_id", cqrVO.getReg_id());
+		rttr.addAttribute("page", scri.getPage());
+		rttr.addAttribute("perPageNum", scri.getPerPageNum());
+		rttr.addAttribute("searchType", scri.getSearchType());
+		rttr.addAttribute("keyword", scri.getKeyword());
+		
+		return "redirect:/qna/readView";
 	}
 
 }
---
+++ pugis/src/main/java/com/kh/pugis/service/consulting/dao/CustomerQuestionListReplyDAO.java
@@ -0,0 +1,20 @@
+package com.kh.pugis.service.consulting.dao;
+
+import java.util.List;
+
+import com.kh.pugis.service.consulting.domain.CustomerQuestionReply;
+
+public interface CustomerQuestionListReplyDAO {
+
+	// 댓글 조회
+	public List<CustomerQuestionReply> readReply(int reg_id) throws Exception;
+	
+	// 댓글 작성
+	public void writdReply(CustomerQuestionReply cqrVO) throws Exception;
+	
+	// 댓글 삭제
+	public void deleteReply(CustomerQuestionReply cqrVO) throws Exception;
+	
+	// 선택된 댓글 조회
+	public CustomerQuestionReply selectReply(int reg_id) throws Exception;
+}
