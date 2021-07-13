a0de8121f6dea0548b55e2c7fc74d62a6bd6869f
YOUNGSIM <imyoung0127@gmail.com>
2021-07-13 19:06:08 +0900

    매출 컨트롤러 엑셀부분 수정

--- pugis/src/main/java/com/kh/pugis/service/consulting/controller/SalesDataController.java
+++ pugis/src/main/java/com/kh/pugis/service/consulting/controller/SalesDataController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.pugis.service.consulting.service.SalesDataExcelService;
 import com.kh.pugis.service.consulting.service.SalesDataService;
 
 /**
@@ -17,6 +19,9 @@ import com.kh.pugis.service.consulting.service.SalesDataService;
 public class SalesDataController {
 	@Autowired
 	SalesDataService sds;
+	
+	@Autowired
+	SalesDataExcelService sdes;
 	
 	@RequestMapping(value = "/totallist")
 	public String totallist(HttpServletRequest req, Model model) {
